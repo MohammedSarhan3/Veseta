fa8372f7682adea05b84117877cc45f82c869bb9
pythone-developer <saraheeeen@gmail.com>
2023-01-25 19:18:46 +0200

    Creat Profile Models

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,12 @@ from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 class Profile(models.Model):
     user=models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
+    name = models.CharField(_("الإسم :"), max_length=50)
+    who_i=models.TextField(_("نبذة عنك:  "),max_length=500)
+    price=models.IntegerField(_("السعر: "))
+    
+    
+    
     class Meta:
         verbose_name = 'Profile'
         verbose_name_plural = 'Profiles'
