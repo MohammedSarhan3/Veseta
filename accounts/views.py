0a6662c07215451e46eaec4ffb68c0fc32a7bd79
pythone-developer <saraheeeen@gmail.com>
2023-02-04 11:17:14 +0200

    Add user_login to Views.py

--- accounts/views.py
+++ accounts/views.py
@@ -38,4 +38,20 @@ def Signup(request):
     return render(request,"user/signup.html",{
         'form':form
     })
-   
+    
+def user_login(request):
+    if request.method=='POST':
+        form=LoginForm()
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('accounts:doctors_list')
+    else:
+        form=LoginForm()
+
+    return render(request,"user/login.html",{
+        'form':form
+    })
+    
